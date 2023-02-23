65e21fb413efcf6abd2053ea5b02e451a6b86ac2
johaortiz <surf234@outlook.es>
2023-02-23 20:22:07 -0300

    Second Good Deploy

--- client/src/helpers/requests.ts
+++ client/src/helpers/requests.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
+// const BASE_URL = "http://localhost:3001"
 
 //Register User
 export const saveUser = async (data: string[]) => {
